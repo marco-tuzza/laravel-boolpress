4b4dcf2e292e87814a585a25264427ec8e1830c7
Marco Tuzza <marcotuzza@hotmail.it>
2020-07-13 20:21:59 +0200

    CRUD completate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>Hello World</h1>
+            <h1>Benvenuto su Infrizzy</h1>
         </div>
     </div>
 </div>
